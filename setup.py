5a926cf8b3a35fa70dd0301e1eae4bf8d3ca397d
swat30 <jacobgodin@gmail.com>
2015-01-13 12:44:10 -0400

    Switch over to PBR for setup

--- setup.py
+++ setup.py
@@ -1,30 +1,11 @@
 #!/usr/bin/env python
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 from docrane import __version__ as version
 
-packages = [
-    'docrane'
-]
-
 setup(
-    name='docrane',
+    setup_requires=['pbr'],
+    pbr=True,
     version=version,
-    description='Manage docker container runtime configuration with etcd',
-    long_description='',
-    author='Jacob Godin',
-    author_email='jacobgodin@gmail.com',
-    url='https://github.com/CloudBrewery/docrane',
-    packages=packages,
-    scripts=['bin/docrane'],
-    classifiers=(
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-    ),
 )
